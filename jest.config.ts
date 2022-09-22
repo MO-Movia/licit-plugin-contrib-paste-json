3ea6fb2fde3612fc99dce22d23c221a7894b2ef2
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2022-09-22 18:51:57 +0530

    - remove unused jest-canvas-mock.

--- jest.config.ts
+++ jest.config.ts
@@ -127,7 +127,7 @@ export default {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['../jest.setup.js', 'jest-canvas-mock'],
+  setupFiles: ['../jest.setup.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['jest-prosemirror/environment'],
