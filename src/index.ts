53f2384d7b413e5f81cff83192c891834f3d18e7
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2022-09-29 14:12:40 +0530

    - FIX: Editor crashes with other plugins with appendTransactions.

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ export class PasteJSONPlugin extends Plugin {
         newState: EditorState
       ) {
         (this as PasteJSONPlugin).schema = newState.schema;
-        return newState.tr;
+        return undefined;
       },
     });
   }
