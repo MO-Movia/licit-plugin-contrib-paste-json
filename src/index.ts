3f2efd8f0e48a19c38adc9035cace80e9394876e
Ashfaq Shamsudeen <ashfaq.shamsudeen@firmusoft.co.in>
2023-02-06 17:01:12 +0530

    - FIX: Missing reference id when pasting JSON.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,38 @@
-import {Schema, Slice} from 'prosemirror-model';
+import {Fragment, Schema, Slice} from 'prosemirror-model';
 import {EditorState, Plugin, PluginKey, Transaction} from 'prosemirror-state';
 import {EditorView} from 'prosemirror-view';
 
 export class PasteJSONPlugin extends Plugin {
   schema: Schema = null;
+  slice: Slice = null;
   constructor() {
     super({
       key: new PluginKey('pasteJSON'),
       props: {
+        transformPastedText(text: string): string {
+          try {
+            const json = JSON.parse(text);
+
+            if (json) {
+              (this as PasteJSONPlugin).slice = new Slice(
+                Fragment.from(
+                  (this as PasteJSONPlugin).schema.nodeFromJSON(json)
+                ),
+                0,
+                0
+              );
+            }
+          } catch (_e) {
+            /* empty */
+          }
+          return text;
+        },
         transformPasted(slice: Slice) {
           if (0 < slice.content.childCount) {
-            try {
-              const json = JSON.parse(slice.content.firstChild.textContent);
-
-              if (json) {
-                slice = Slice.fromJSON((this as PasteJSONPlugin).schema, json);
-              }
-            } catch (_e) {
-              /* empty */
-            }
+            slice = (this as PasteJSONPlugin).slice;
           }
+          // reset
+          (this as PasteJSONPlugin).slice = null;
           return slice;
         },
       },
